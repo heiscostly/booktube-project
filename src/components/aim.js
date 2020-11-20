9aa566795404e7c79056157974e6cb63646175b0
theboycostly <ayangcostly@gmail.com>
2020-11-20 17:22:32 +0100

    Making Changes to styles

--- src/components/aim.js
+++ src/components/aim.js
@@ -10,18 +10,18 @@ export default function aim() {
             <div className="aim__stuffs">
                 <div className="aims">
                     <img className="fire__img" src={fireworks} alt="fire"/>
-                    <h3>We Partner with the Hottest Book Companies</h3>
-                    <p>We're talking Evernote,Mailchimp, Depositphotos just to name a few.</p>
+                    <h3 className="text-center">We Partner with the Hottest Book Companies</h3>
+                    <p className="text-center">We're talking Evernote,Mailchimp, Depositphotos just to name a few.</p>
                 </div>
                 <div className="aims">
                     <img className="fire__img" src={agree} alt="fire"/>
-                    <h3>We Run Deals on their Products</h3>
-                    <p>But don't sleep on ot! Our deals sell out within a couples of weeks</p>
+                    <h3 className="text-center">We Run Deals on their Products</h3>
+                    <p className="text-center">But don't sleep on ot! Our deals sell out within a couples of weeks</p>
                 </div>
                 <div className="aims">
                     <img className="fire__img" src={bitcoin} alt="fire"/>
-                    <h3>You save thousands of dollars</h3>
-                    <p>And build up a full arsenal of tools to grow your business</p>
+                    <h3 className="text-center">You save thousands of dollars</h3>
+                    <p className="text-center">And build up a full arsenal of tools to grow your business</p>
                 </div>
             </div>
         </div>
